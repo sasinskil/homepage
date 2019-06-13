b0d541cd76330dcc443db5f4cd34d54dd893c3f3
sasinskil <sasinskil@gmail.com>
2019-06-13 23:02:40 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//alert('Witaj!');
+//console.log('Witaj kolego!');
+
+const name = 'Łukasz Sasiński';
+const age = 24;
+
+//console.log(name);
+//console.log(age);
+
+console.log(`Witaj ! Nazywam się ${name}, mam ${age} lata - zaglądasz tutaj, nawet nie wiesz jak się cieszę !`);
