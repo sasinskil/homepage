c82c7bee8ce98f5b858600afcc6c83e1e5c79265
sasinskil <sasinskil@gmail.com>
2019-06-16 14:14:25 +0200

    DOM:JS - document.querySelector/element.innerHTML

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const name = 'Łukasz Sasiński';
+const name = 'Łukasz';
 const age = 24;
 
 console.log(`Witaj ! Nazywam się ${name}, mam ${age} lata - miło, że tutaj zaglądasz!`);
+
+const aboutHeader = document.querySelector('.about__header');
+
+console.log(aboutHeader);
+console.log(aboutHeader.innerHTML);
+
+aboutHeader.innerHTML = `Kim jest ${name} ?`;
