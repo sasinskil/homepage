83235a77407c773746b7bf554465f188bf50ebca
sasinskil <sasinskil@gmail.com>
2019-06-16 14:03:17 +0200

    using BEM - refactoring

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-//alert('Witaj!');
-//console.log('Witaj kolego!');
-
 const name = 'Łukasz Sasiński';
 const age = 24;
-
-//console.log(name);
-//console.log(age);
 
 console.log(`Witaj ! Nazywam się ${name}, mam ${age} lata - miło, że tutaj zaglądasz!`);
