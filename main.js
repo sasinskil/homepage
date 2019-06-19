99844450f0b279d5e5c5cd32caecab0a9a5b9bd8
sasinskil <sasinskil@gmail.com>
2019-06-20 00:20:59 +0200

    using block__element--js js modifier when using with js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const age = 24;
 
 console.log(`Witaj ! Nazywam się ${name}, mam ${age} lata - miło, że tutaj zaglądasz!`);
 
-const aboutHeader = document.querySelector('.about__header');
+const aboutHeader = document.querySelector('.about__header--js');
 
 console.log(aboutHeader);
 console.log(aboutHeader.innerHTML);
