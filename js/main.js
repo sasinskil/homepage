e09cf29a551bd5f03f64a0c3c856be72ed70a377
sasinskil <sasinskil@gmail.com>
2019-06-23 08:23:39 +0200

    arrow function es6 - welcome

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 const name = 'Łukasz';
 const age = 24;
 
-console.log(`Witaj ! Nazywam się ${name}, mam ${age} lata - miło, że tutaj zaglądasz!`);
-
 const aboutHeader = document.querySelector('.about__header--js');
 
 console.log(aboutHeader);
 console.log(aboutHeader.innerHTML);
 
 aboutHeader.innerHTML = `Kim jest ${name} ?`;
+
+const welcome = (name, age) => {
+    if(age <18) {
+        console.log('Ups, nie jestem pełnoletni, nie mogę udzielić Ci pewnych informacji ale miło, że tu zaglądasz');   
+    } else {
+        console.log(`Witaj ! Nazywam się ${name}, mam ${age} lata - miło, że tutaj zaglądasz!`);
+    }
+}
+
+console.log(welcome('Łukasz',24));
