01fc2cf4410c7a6ccdab054539ad53dea851e460
sasinskil <sasinskil@gmail.com>
2019-07-03 03:07:28 +0200

    burger menu

--- js/main.js
+++ js/main.js
@@ -9,11 +9,24 @@ console.log(aboutHeader.innerHTML);
 aboutHeader.innerHTML = `Kim jest ${name} ?`;
 
 const welcome = (name, age) => {
-    if(age <18) {
-        console.log('Ups, nie jestem pełnoletni, nie mogę udzielić Ci pewnych informacji ale miło, że tu zaglądasz');   
+    if (age < 18) {
+        console.log('Ups, nie jestem pełnoletni, nie mogę udzielić Ci pewnych informacji ale miło, że tu zaglądasz');
     } else {
         console.log(`Witaj ! Nazywam się ${name}, mam ${age} lata - miło, że tutaj zaglądasz!`);
     }
 }
 
-welcome(name,age);
+welcome(name, age);
+
+
+const button = document.querySelector('.navigation__button--js');
+const navigationList = document.querySelector('.navigation__list');
+
+button.addEventListener('click', (e) => {
+    navigationList.classList.toggle('navigation__list--visible');
+    if (navigationList.classList.contains('navigation__list--visible')) {
+        button.innerHTML = 'X';
+    } else {
+        button.innerHTML = '☰';
+    }
+});
